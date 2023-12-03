4e5c9723904b4914325b23225bd916de99aef8ee
AalamTasmatov <elbrusuuluruslan007@gmail.com>
2023-12-03 20:25:55 +0600

    commit 2

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
-const wrapper = document.querySelector(".wrapper1")
-const modal_login = document.querySelector(".modal_login")
-const mBtn = document.querySelector(".modal_login_form-button")
-const sign_out = document.querySelector("#sign_out")
+let wrapper = document.querySelector(".wrapper1")
+let modal_login = document.querySelector(".modal_login")
+let modal_signin = document.querySelector(".modal_signin")
+let mBtn = document.querySelector(".modal_login_form-button")
+let sBtn = document.querySelector(".modal_signin_form-button")
+let toSignin = document.querySelector(".to_sign_in")
+let toLogin = document.querySelector(".to_log_in")
 
-wrapper.style.display="none"
-mBtn.onclick=()=>{
-    wrapper.style.display="block"
-    modal_login.style.display="none"
+
+
+async function getData(){
+    let data1 = await fetch("https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks")
+    .then(response => response.json()).then(data => data)
+    return data1
 }
 
-sign_out.onclick=()=>{
-    wrapper.style.display="none"
-    modal_login.style.display="block"
+async function postData(){
+
 }
+
+
+
+const wLogin = () =>{
+    modal_login.style.display = "flex"
+    modal_signin.style.display = "none"
+    wrapper.style.display = "none"
+    
+}
+
+wLogin()
+
+const wSignin = () =>{
+    modal_login.style.display = "none"
+    modal_signin.style.display = "flex"
+    wrapper.style.display = "none"
+}
+const wWrapper = () => {
+    modal_login.style.display = "none"
+    modal_signin.style.display =  "none"
+    wrapper.style.display ="block"
+}
+
+
+const mInpLogin = document.querySelector(".modal_login_form-input1")
+const mInpPassword = document.querySelector(".modal_login_form-input2")
+mBtn.onclick = async()=>{
+    const data = await getData()
+    let answer= 0 
+    data.map((item)=>{
+        if(item.email == mInpLogin.value && item.password == mInpPassword.value){
+            answer = item
+        }
+    })
+    if(answer!==0){
+        wWrapper()
+    }
+}
+
+toSignin.onclick=()=>{
+    wSignin()
+}
+
+toLogin.onclick=()=>{
+    wLogin()
+}
+
+sBtn.onclick = async() => {
+    const url = 'https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks';
+    const data = {
+      email: 'value1',
+      password: 'value2'
+    };
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any other headers if needed
+        },
+        body: JSON.stringify(data)
+      });
+  
+      const responseData = await response.json();
+      console.log('Success:', responseData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
