5504ef369ba9461d82ebce2fb1e537a457031a8d
AalamTasmatov <elbrusuuluruslan007@gmail.com>
2023-12-11 23:00:27 +0600

    ocherednoy commit

--- index.js
+++ index.js
@@ -1,65 +1,109 @@
-let wrapper = document.querySelector(".wrapper1");
-let modal_login = document.querySelector(".modal_login");
-let modal_signin = document.querySelector(".modal_signin");
-let mBtn = document.querySelector(".modal_login_form-button");
-let sBtn = document.querySelector(".modal_signin_form-button");
-let toSignin = document.querySelector(".to_sign_in");
-let toLogin = document.querySelector(".to_log_in");
-const dashboard = document.querySelector(".main__inner");
+const select = (selector) => document.querySelector(selector);
+const create = (tag) => document.createElement(tag);
 
+const wrapper = select(".wrapper1");
+const wrapperExt = select(".wrapper2")
+const modalLogin = select(".modal_login");
+const modalSignin = select(".modal_signin");
+const mBtn = select(".modal_login_form-button");
+const sBtn = select(".modal_signin_form-button");
+const toSignin = select(".to_sign_in");
+const toLogin = select(".to_log_in");
+const dashboard = select(".main__inner");
 
-
-
-const filterSelect = document.createElement('select');
+const filterSelect = create('select');
 filterSelect.innerHTML = `
   <option value="all">All Tasks</option>
   <option value="current">Current Tasks</option>
   <option value="completed">Completed Tasks</option>
 `;
 
-// Добавляем select на страницу
-wrapper.appendChild(filterSelect);
+wrapperExt.appendChild(filterSelect);
 
-// Добавляем обработчик событий для фильтрации
 filterSelect.addEventListener('change', async () => {
   const selectedValue = filterSelect.value;
   await filterTasks(selectedValue);
 });
 
-// Функция фильтрации тасков
-async function filterTasks(filterType) {
-  dashboard.innerHTML = ""; // Очищаем текущий список
+dashboard.addEventListener('click', async (event) => {
+  const selectedFilter = filterSelect.value;
 
-  let newData = await toShowTasks(idshnik);
+  if (selectedFilter === 'all') {
+    if (event.target.classList.contains('mark_btn')) {
+      const taskId = event.target.dataset.taskId;
+      const completed = event.target.dataset.completed === 'true';
+
+      await markTaskAsCompleted(taskId, idshnik, !completed);
+      dashboard.innerHTML = "";
+      await appendDash();
+    } else if (event.target.classList.contains('todo_btn')) {
+      const taskId = event.target.dataset.taskId;
+
+      await editTask(taskId, idshnik);
+    } else if (event.target.classList.contains('delete_btn')) {
+      const taskId = event.target.dataset.taskId;
+      await deleteTask(taskId, idshnik);
+    }
+  }
+});
+
+async function filterTasks(filterType) {
+  dashboard.innerHTML = "";
+
+  let allTasks = await toShowTasks(idshnik);
 
   if (filterType === 'current') {
-    newData = newData.filter(item => !item.completed);
+    let currentTasks = allTasks.filter(item => !item.completed);
+    renderTasks(currentTasks);
   } else if (filterType === 'completed') {
-    newData = newData.filter(item => item.completed);
+    let completedTasks = allTasks.filter(item => item.completed);
+    renderTasks(completedTasks);
+  } else {
+    renderTasks(allTasks);
   }
 
-  newData.map((item) => {
-    let newModel = document.createElement("div");
-    newModel.setAttribute("class", "todo ");
+  const markButtons = document.querySelectorAll('.mark_btn');
+  markButtons.forEach((button) => {
+    button.style.display = filterType === 'all' ? 'block' : 'none';
+  });
+}
+
+function renderTasks(tasks) {
+  tasks.forEach((item) => {
+    let newModel = create("div");
+    newModel.classList.add("todo");
     newModel.innerHTML = `<h3>${item.title}</h3>
-                              <p>${item.body}</p>
-                              <button class="todo_btn" data-task-id="${item.id}">edit</button>
-                              <button class="delete_btn" data-task-id="${item.id}">delete</button>`;
+                          <p>${item.body}</p>`;
+
+    if (filterSelect.value === 'all') {
+      newModel.innerHTML += `<button class="todo_btn" data-task-id="${item.id}">edit</button>
+                             <button class="delete_btn" data-task-id="${item.id}">delete</button>`;
+    }
+
+    newModel.innerHTML += `<button class="mark_btn" data-task-id="${item.id}" data-completed="${item.completed}">
+                            ${item.completed ? 'Mark as Undone' : 'Marked as Done'}
+                          </button>`;
+
     dashboard.append(newModel);
   });
-}  
-
+}
 
 async function getData() {
-  let data1 = await fetch("https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks")
-    .then(response => response.json()).then(data => data);
-  return data1;
+  try {
+    const response = await fetch("https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks");
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 
 async function toShowTasks(id) {
-  let data1 = await fetch(`https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks/${id}/tasks`)
-    .then(response => response.json()).then(data => data);
-  return data1;
+  try {
+    const response = await fetch(`https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks/${id}/tasks`);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+  }
 }
 
 async function createTask(title, body, id) {
@@ -84,23 +128,26 @@ async function createTask(title, body, id) {
 }
 
 const wLogin = () => {
-  modal_login.style.display = "flex";
-  modal_signin.style.display = "none";
+  modalLogin.style.display = "flex";
+  modalSignin.style.display = "none";
   wrapper.style.display = "none";
+  clearErrorMessages();
 }
 
 wLogin();
 
 const wSignin = () => {
-  modal_login.style.display = "none";
-  modal_signin.style.display = "flex";
+  modalLogin.style.display = "none";
+  modalSignin.style.display = "flex";
   wrapper.style.display = "none";
+  clearErrorMessages();
 }
 
 const wWrapper = () => {
-  modal_login.style.display = "none";
-  modal_signin.style.display = "none";
+  modalLogin.style.display = "none";
+  modalSignin.style.display = "none";
   wrapper.style.display = "block";
+  clearErrorMessages();
 }
 
 let idshnik = 0;
@@ -108,34 +155,30 @@ let idshnik = 0;
 async function appendDash() {
   let newData = await toShowTasks(idshnik);
 
-  await newData.map((item) => {
-    let newModel = document.createElement("div");
-    newModel.setAttribute("class", "todo ");
+  newData.map((item) => {
+    let newModel = create("div");
+    newModel.classList.add("todo");
     newModel.innerHTML = `<h3>${item.title}</h3>
                               <p>${item.body}</p>
                               <button class="todo_btn" data-task-id="${item.id}">edit</button>
                               <button class="delete_btn" data-task-id="${item.id}">delete</button>
-                              <button class="mark_btn" data-task-id="${item.id}" data-completed="${item.completed}">${item.completed ? 'Mark as Undone' : 'Mark as Done'}</button>`;
+                              <button class="mark_btn" data-task-id="${item.id}" data-completed="${item.completed}">
+                                ${item.completed ? 'Mark as Undone' : 'Marked as Done'}
+                              </button>`;
+
     dashboard.append(newModel);
   });
 
-  // Добавляем обработчик событий для кнопки "Mark as Done/Undone"
   dashboard.addEventListener('click', async (event) => {
     if (event.target.classList.contains('mark_btn')) {
       const taskId = event.target.dataset.taskId;
       const completed = event.target.dataset.completed === 'true';
 
-      // Меняем значение completed в таске
       await markTaskAsCompleted(taskId, idshnik, !completed);
-
-      // Обновляем список
-      dashboard.innerHTML = "";
-      await appendDash();
     }
   });
 }
 
-// Функция пометки таска как сделанный/не сделанный
 async function markTaskAsCompleted(id, userId, completed) {
   const data = {
     completed: completed,
@@ -151,21 +194,26 @@ async function markTaskAsCompleted(id, userId, completed) {
 
     const responseData = await response.json();
     console.log('Mark as Completed Success:', responseData);
+
+    const taskElement = document.querySelector(`.todo[data-task-id="${id}"]`);
+    if (taskElement) {
+      let markBtn = taskElement.querySelector('.mark_btn');
+      if (markBtn) {
+        markBtn.textContent = completed ? 'Mark as Undone' : 'Marked as Done';
+        markBtn.dataset.completed = completed;
+      } else {
+        markBtn = create("button");
+        markBtn.classList.add("mark_btn");
+        markBtn.dataset.taskId = id;
+        markBtn.dataset.completed = completed;
+        markBtn.textContent = completed ? 'Mark as Undone' : 'Marked as Done';
+        taskElement.appendChild(markBtn);
+      }
+    }
   } catch (error) {
     console.error('Mark as Completed Error:', error);
   }
 }
-
-// Добавляем обработчик событий на родительский элемент
-dashboard.addEventListener('click', async (event) => {
-  if (event.target.classList.contains('todo_btn')) {
-    const taskId = event.target.dataset.taskId;
-    await editTask(taskId, idshnik);
-  } else if (event.target.classList.contains('delete_btn')) {
-    const taskId = event.target.dataset.taskId;
-    await deleteTask(taskId, idshnik);
-  }
-});
 
 async function deleteTask(id, userId) {
   try {
@@ -183,21 +231,15 @@ async function deleteTask(id, userId) {
   await appendDash();
 }
 
-
-// Добавляем обработчик событий на родительский элемент
 dashboard.addEventListener('click', async (event) => {
-  // Проверяем, была ли нажата кнопка "edit"
   if (event.target.classList.contains('todo_btn')) {
-    // Получаем ID таска из атрибута данных (data-task-id) кнопки
     const taskId = event.target.dataset.taskId;
-
-    // Вызываем функцию editTask с передачей ID таска
     await editTask(taskId, idshnik);
   }
 });
 
-const mInpLogin = document.querySelector(".modal_login_form-input1");
-const mInpPassword = document.querySelector(".modal_login_form-input2");
+const mInpLogin = select(".modal_login_form-input1");
+const mInpPassword = select(".modal_login_form-input2");
 
 let WORK = async () => {
   const data = await getData();
@@ -211,11 +253,15 @@ let WORK = async () => {
 
   if (answer !== 0) {
     wWrapper();
+  } else {
+    displayErrorMessage("login-error-message", "Неправильный логин или пароль. Пожалуйста, попробуйте снова.");
   }
   await appendDash();
 }
 
-mBtn.onclick = () => { WORK(); }
+mBtn.onclick = () => {
+  WORK();
+}
 
 toSignin.onclick = () => {
   wSignin();
@@ -225,62 +271,44 @@ toLogin.onclick = () => {
   wLogin();
 }
 
-const sInpEmail = document.querySelector(".modal_signin_form-input1");
-const sInpPassw = document.querySelector(".modal_signin_form-input2");
+const sInpEmail = select(".modal_signin_form-input1");
+const sInpPassw = select(".modal_signin_form-input2");
 
 sBtn.onclick = async () => {
-  const url = 'https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks';
-  const data = {
-    email: sInpEmail.value,
-    password: sInpPassw.value
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    });
-
-    const responseData = await response.json();
-    console.log('Success:', responseData);
-  } catch (error) {
-    console.error('Error:', error);
+  if (!sInpPassw.value) {
+    displayErrorMessage("signin-error-message", "Введите пароль.");
+  } else {
+    // Остальной код
   }
-  wLogin();
 };
 
-const creaInp1 = document.querySelector(".newTaskInp1");
-const creaInp2 = document.querySelector(".newTaskInp2");
-const creaBtn = document.querySelector(".newTaskBtn");
+const creaInp1 = select(".newTaskInp1");
+const creaInp2 = select(".newTaskInp2");
+const creaBtn = select(".newTaskBtn");
 
 creaBtn.onclick = async () => {
   await createOrUpdateTask(creaInp1, creaInp2, idshnik);
 }
 
-const creaInp3 = document.querySelector(".newTaskInp3");
-const creaEditBtn = document.querySelector(".newTaskEditBtn");
+const creaInp3 = select(".newTaskInp3");
+const creaEditBtn = select(".newTaskEditBtn");
 
 creaEditBtn.onclick = async () => {
   const taskId = creaInp3.value;
   if (taskId) {
     await editTask(taskId, idshnik);
   } else {
-    alert("Please enter a task ID.");
+    alert("Введите ID задачи.");
   }
 };
 
-
-const editBtn = document.querySelector(".todo_btn")
-
-
 async function editTask(id, userId) {
-  const newTitle = prompt("Enter new title:");
-  const newBody = prompt("Enter new body:");
+  const newTitle = prompt("Введите новый заголовок:");
+  const newBody = prompt("Введите новое описание:");
   await updateTask(id, newTitle, newBody, userId);
+
   dashboard.innerHTML = "";
+
   await appendDash();
 }
 
@@ -288,7 +316,6 @@ async function updateTask(id, title, body, userId) {
   const data = {
     title: title,
     body: body,
-    
   };
   try {
     const response = await fetch(`https://656c49aee1e03bfd572e26bd.mockapi.io/aalam/todo/users_tasks/${userId}/tasks/${id}`, {
@@ -300,16 +327,26 @@ async function updateTask(id, title, body, userId) {
     });
 
     const responseData = await response.json();
-    console.log('Update Success:', responseData);
+    console.log('Обновление выполнено успешно:', responseData);
   } catch (error) {
-    console.error('Update Error:', error);
+    console.error('Ошибка при обновлении:', error);
   }
 }
 
 async function createOrUpdateTask(title, body, userId) {
-  
   await createTask(title, body, userId);
   dashboard.innerHTML = "";
   await appendDash();
 }
-  
+
+function displayErrorMessage(elementId, message) {
+  const errorDiv = select(`.${elementId}`);
+  if (errorDiv) {
+    errorDiv.textContent = message;
+  }
+}
+
+function clearErrorMessages() {
+  displayErrorMessage("login-error-message", "");
+  displayErrorMessage("signin-error-message", "");
+}
